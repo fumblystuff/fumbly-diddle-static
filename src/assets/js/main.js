ac843938bbc955f5e1a1b73d9eda57a5503e14da
johnwargo <john@johnwargo.com>
2024-02-22 10:36:56 -0500

    working away

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,15 +1,14 @@
 /*
-	Drift by Pixelarity
+	Vortex by Pixelarity
 	pixelarity.com | hello@pixelarity.com
 	License: pixelarity.com/license
 */
 
 (function($) {
 
-	var $window = $(window),
-		$body = $('body'),
-		$banner = $('#banner'),
-		$header = $('#header');
+	var	$window = $(window),
+		$header = $('#header'),
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
@@ -17,7 +16,8 @@
 			large:    [ '981px',   '1280px' ],
 			medium:   [ '737px',   '980px'  ],
 			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ]
+			xsmall:   [ '361px',   '480px'  ],
+			xxsmall:  [ null,      '360px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -27,51 +27,48 @@
 			}, 100);
 		});
 
-	// Scrolly links.
-		$('.scrolly').scrolly({
-			offset: function() { return $header.outerHeight() - 10; }
-		});
+	// Tweaks/fixes.
 
-	// Header.
-	// If the header is using "alt" styling and #banner is present, use scrollwatch
-	// to revert it back to normal styling once the user scrolls past the banner.
-		if ($header.hasClass('alt')
-		&&	$banner.length > 0) {
+		// Polyfill: Object fit.
+			if (!browser.canUse('object-fit')) {
 
-			$window.on('resize', function() { $window.trigger('scroll'); });
+				$('.image[data-position]').each(function() {
 
-			$banner.scrollex({
-				bottom:		$header.outerHeight() + 5,
-				terminate:	function() { $header.removeClass('alt'); },
-				enter:		function() { $header.addClass('alt'); },
-				leave:		function() { $header.removeClass('alt'); $header.addClass('reveal'); }
-			});
+					var $this = $(this),
+						$img = $this.children('img');
 
-		}
+					// Apply img as background.
+						$this
+							.css('background-image', 'url("' + $img.attr('src') + '")')
+							.css('background-position', $this.data('position'))
+							.css('background-size', 'cover')
+							.css('background-repeat', 'no-repeat');
+
+					// Hide img.
+						$img
+							.css('opacity', '0');
+
+				});
+
+			}
 
 	// Dropdowns.
 		$('#nav > ul').dropotron({
-			alignment: 'right'
+			alignment: 'right',
+			hideDelay: 350,
+			baseZIndex: 100000
 		});
 
-	// Nav Panel.
+	// Menu.
+		$('<a href="#navPanel" class="navPanelToggle">Menu</a>')
+			.appendTo($header);
 
-		// Title Bar.
-			$(
-				'<div id="navButton">' +
-					'<a href="#navPanel" class="toggle"></a>' +
-				'</div>'
-			)
-				.appendTo($body);
-
-		// Panel.
-			$(
-				'<div id="navPanel">' +
-					'<nav>' +
-						$('#nav').navList() +
-					'</nav>' +
-				'</div>'
-			)
+		$(	'<div id="navPanel">' +
+				'<nav>' +
+					$('#nav') .navList() +
+				'</nav>' +
+				'<a href="#navPanel" class="close"></a>' +
+			'</div>')
 				.appendTo($body)
 				.panel({
 					delay: 500,
@@ -79,39 +76,7 @@
 					hideOnSwipe: true,
 					resetScroll: true,
 					resetForms: true,
-					side: 'left',
-					target: $body,
-					visibleClass: 'navPanel-visible'
+					side: 'right'
 				});
-
-	// Slider.
-		var $sliders = $('.slider');
-
-		if ($sliders.length > 0) {
-
-			$sliders.slidertron({
-				mode: 'fadeIn',
-				seamlessWrap: true,
-				viewerSelector: '.viewer',
-				reelSelector: '.viewer .reel',
-				slidesSelector: '.viewer .reel .slide',
-				advanceDelay: 0,
-				speed: 400,
-				fadeInSpeed: 1000,
-				autoFit: true,
-				autoFitAspectRatio: (840 / 344),
-				navPreviousSelector: '.nav-previous',
-				navNextSelector: '.nav-next',
-				indicatorSelector: '.indicator ul li',
-				slideLinkSelector: '.link'
-			});
-
-			$window
-				.on('resize load', function() {
-					$sliders.trigger('slidertron_reFit');
-				})
-				.trigger('resize');
-
-		}
 
 })(jQuery);
