b0fcb5a72ee5cb9ff2b91d480750a5852218a2a7
johnwargo <john@johnwargo.com>
2024-03-24 08:56:32 -0400

    added sort to the product list

--- src/products.11tydata.js
+++ src/products.11tydata.js
@@ -4,7 +4,7 @@ module.exports = {
   eleventyComputed: {
     eleventyNavigation: {
       url: (data) => {
-        const productCollection = data.collections.product;
+        const productCollection = data.collections.productList;
         if (productCollection.length > 0) {
           return productCollection[0].page.url;
         }
