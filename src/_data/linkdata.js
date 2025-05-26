35c453ac9ea93b344db6d1b0a57c384b4ec84379
johnwargo <john@johnwargo.com>
2025-05-26 15:27:43 -0400

    pathbackup update

--- src/_data/linkdata.js
+++ src/_data/linkdata.js
@@ -1,12 +1,16 @@
 module.exports = {
   PathBackup: [
     {
+      version: "0.1.5",
+      installerURL: "https://fumblydiddle.b-cdn.net/pathbackup/PathBackupSetup-0.1.5.exe",
+      date: "2025-05-26",
+      note: "Fixed incorrect text in JSON backup success message"
+    }, {
       version: "0.1.3",
       installerURL: "https://fumblydiddle.b-cdn.net/pathbackup/PathBackupSetup-0.1.3.exe",
       date: "2024-06-20",
       note: "Minor bug fixes"
-    },
-    {
+    }, {
       version: "0.1.1",
       installerURL: "https://fumblydiddle.b-cdn.net/pathbackup/PathBackupSetup-0.1.1.exe",
       date: "2024-03-30",
