fe4995ecdf7b23ff0b1a3c803695bd9ca66843ae
johnwargo <john@johnwargo.com>
2025-06-14 08:37:11 -0400

    Update linkdata.js

--- src/_data/linkdata.js
+++ src/_data/linkdata.js
@@ -20,7 +20,7 @@ module.exports = {
   SubCopy: [
     {
       version: "1.1.1.1",
-      installerURL: "https://fumblydiddle.b-cdn.net/subcopy/SubCopySetup-1.1.1.exe",
+      installerURL: "https://fumblydiddle.b-cdn.net/subcopy/SubCopySetup-1.1.1.1.exe",
       date: "2025-06-14",
       note: "Added support for projects"
     }, {
